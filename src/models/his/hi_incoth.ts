85117591701ce94fd91b3bec4f0f8a623c4cd170
thawatchai <thawatchai.sea2@gmail.com>
2020-03-17 14:14:33 +0700

    o

--- src/models/his/hi_incoth.ts
+++ src/models/his/hi_incoth.ts
@@ -25,6 +25,65 @@ export class HiIncothModel {
       WHERE o.vn = '${vn}'`);
     return data;
   }
+  async getIncothSelect(db: knex) {
+    let data = await db.raw(`
+      SELECT * FROM debtor_incoth WHERE accept = 0;
+      `);
+    return data;
+  }
+
+  async getDebtorIncoth(db: knex, startdate: any, enddate: any) {
+    let data = await db.raw(`
+      select 
+      '10957' as hospcode
+      ,concat(p.fname,' ',p.lname) as fullname
+      ,o.vn
+      ,o.hn
+      ,o.pttype
+      ,t.namepttype
+      ,s.hospmain
+      ,cast(p.pop_id as char(13)) as cid
+      ,ifnull((select cast(group_concat(icd10) as char(255)) from hi.ovstdx as x where o.vn=x.vn),(select cast(group_concat(icdda) as char(255)) from hi.dt inner join hi.dtdx on dtdx.dn=dt.dn where o.vn=dt.vn) ) as diagnosis
+      ,ifnull((select group_concat(icd9cm) from hi.oprt as r where o.vn=r.vn),'') as procedure_opd
+      ,date_format(o.vstdttm,'%Y-%m-%d') as date_serv
+      ,ifnull(sum(case i.income when '01' then i.rcptamt end),0) as lab
+      ,ifnull(sum(case i.income when '03' then i.rcptamt end),0) as sp
+      ,ifnull(sum(case i.income when '04' then i.rcptamt end),0) as proc
+      ,ifnull(sum(case i.income when '14' then i.rcptamt end),0) as item
+      ,ifnull(sum(case i.income when '02' then i.rcptamt end),0) as xray
+      ,ifnull(sum(case when i.income in ('08','09','10','11') then i.rcptamt end),0) as drug
+      ,ifnull(sum(case when i.income in ('06','07') then i.rcptamt end),0) as rehab
+      ,sum(case when i.income not in ('01','02','03','04','06','07','08','09','10','11','14') then i.rcptamt end) as other
+      ,sum(i.rcptamt) as total
+      from 
+      hi.ovst as o 
+      inner join hi.pt as p on o.hn=p.hn 
+      inner join hi.incoth as i on o.vn=i.vn and o.an=0 
+      inner join hi.pttype as t on o.pttype=t.pttype and t.inscl = 'AAA' 
+      left join hi.insure as s on o.pttype=s.pttype and o.hn=s.hn 
+      left join hi.hospcode as c on s.hospmain=c.off_id 
+      where date_format(o.vstdttm,'%Y-%m-%d') BETWEEN date_format( '${startdate}' ,'%Y-%m-%d') and date_format( '${enddate}' ,'%Y-%m-%d')
+      and o.pttype in ( '25','38')
+      group by o.vn
+      order by hospmain
+      `);
+    return data[0];
+  }
+
+  insertDebtorIncoth(db: knex, info: any) {
+    console.log(info.vn);
+
+    return db('debtor_incoth')
+      .whereNot('vn', info.vn)
+      .insert(info);
+  }
+
+  updateDebtorIncoth(db: knex, vn: any, info: any) {
+    console.log(info);
+    let sql = `update debtor_incoth set accept = ${info.accept} ,status = '${info.status}' where vn = ${vn}`;
+    return db.raw(sql);
+
+  }
 
   update(db: knex, id: any, info: any) {
     return db(this.tableName)
@@ -35,10 +94,6 @@ export class HiIncothModel {
   updateIncoth(db: knex, vn: any, info: any) {
     let sql = `update ${this.tableName} set rcptno = ${info.rcptno} where vn = ${vn}`;
     return db.raw(sql);
-
-    //   return db(this.tableName)
-    //     .where('vn', vn)
-    //     .update(info);
   }
 
 }
--- src/routes/his_incoth.ts
+++ src/routes/his_incoth.ts
@@ -2,6 +2,7 @@
 
 import * as Knex from 'knex';
 import * as fastify from 'fastify';
+import * as moment from 'moment'
 
 import { HiIncothModel } from '../models/his/hi_incoth';
 import { HiOvstModel } from '../models/his/hi_ovst'
@@ -13,9 +14,93 @@ const hiOvstModel = new HiOvstModel();
 const router = (fastify, { }, next) => {
 
   var dbHIS: Knex = fastify.dbHIS;
+  var db: Knex = fastify.db;
 
   fastify.get('/', async (req: fastify.Request, reply: fastify.Reply) => {
     reply.code(200).send({ message: 'Welcome to SMART HIS API services!', version: '1.0 build 20190522-1' })
+  });
+
+  fastify.get('/selectDebtor/:startdate/:enddate', async (req: fastify.Request, reply: fastify.Reply) => {
+    const startdate = req.params.startdate;
+    const enddate = req.params.enddate;
+
+    try {
+      const rs: any = await hiIncothModel.getDebtorIncoth(dbHIS, startdate, enddate);
+      console.log(rs);
+
+      if (rs[0]) {
+        rs.forEach(async v => {
+          let info = {
+            hospcode: v.hospcode,
+            fullname: v.fullname,
+            vn: v.vn,
+            hn: v.hn,
+            pttype: v.pttype,
+            namepttype: v.namepttype,
+            hospmain: v.hospmain,
+            cid: v.cid,
+            diagnosis: v.diagnosis,
+            procedure: v.procedure_opd,
+            date_serv: v.date_serv,
+            lab: v.lab,
+            sp: v.sp,
+            proc: v.proc,
+            item: v.item,
+            xray: v.xray,
+            drug: v.drug,
+            rehab: v.rehab,
+            other: v.other,
+            total: v.total,
+            status: 'DEBTOR'
+          }
+          console.log(info);
+          try {
+            const rs: any = await hiIncothModel.insertDebtorIncoth(db, info);
+          } catch (error) {
+            console.log(error);
+            // reply.code(HttpStatus.INTERNAL_SERVER_ERROR).send({ message: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR) })
+          }
+        });
+        reply.code(HttpStatus.OK).send({ info: rs })
+      }
+    } catch (error) {
+      console.log(error);
+      reply.code(HttpStatus.INTERNAL_SERVER_ERROR).send({ message: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR) })
+    }
+  });
+
+  fastify.get('/selectDebtor', async (req: fastify.Request, reply: fastify.Reply) => {
+
+    try {
+      const rs: any = await hiIncothModel.getIncothSelect(db);
+      reply.code(HttpStatus.OK).send({ info: rs[0] })
+    } catch (error) {
+      console.log(error);
+      reply.code(HttpStatus.INTERNAL_SERVER_ERROR).send({ message: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR) })
+    }
+  });
+
+  fastify.post('/insertDebtor', async (req: fastify.Request, reply: fastify.Reply) => {
+    const _info = req.body;
+    try {
+      const rs: any = await hiIncothModel.insertDebtorIncoth(db, _info);
+      reply.code(HttpStatus.OK).send({ info: rs[0] })
+    } catch (error) {
+      console.log(error);
+      reply.code(HttpStatus.INTERNAL_SERVER_ERROR).send({ message: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR) })
+    }
+  });
+
+  fastify.put('/updateDebtor/:vn', async (req: fastify.Request, reply: fastify.Reply) => {
+    const _info = req.body;
+    const vn = req.params.vn;
+    try {
+      const rs: any = await hiIncothModel.updateDebtorIncoth(db, vn, _info);
+      reply.code(HttpStatus.OK).send({ info: rs[0] })
+    } catch (error) {
+      console.log(error);
+      reply.code(HttpStatus.INTERNAL_SERVER_ERROR).send({ message: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR) })
+    }
   });
 
   fastify.get('/infoOvst/:hn/:vstdttm', async (req: fastify.Request, reply: fastify.Reply) => {
